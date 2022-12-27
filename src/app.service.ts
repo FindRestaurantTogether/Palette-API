52e9668143b21222cd63f00bbcb3def9d5d6d89f
w8385 <w8385@naver.com>
2022-12-27 18:27:46 +0900

    Update: elastic v8 & change search dto

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,21 +4,19 @@ import { RestaurantDto } from './restaurant.dto';
 
 @Injectable()
 export class AppService {
-  async search(keyword): Promise<RestaurantDto[]> {
+  async search(keyword): Promise<unknown[]> {
     const query = keyword;
     const elastic = client;
     try {
       const response = await elastic.search({
         index: 'store_list',
-        body: {
-          query: {
-            multi_match: {
-              query,
-            },
+        query: {
+          multi_match: {
+            query,
           },
         },
       });
-      return response.body.hits.hits.map((hit) => hit._source);
+      return response.hits.hits.map((hit) => hit._source);
     } catch (error) {
       console.log(error);
     }
