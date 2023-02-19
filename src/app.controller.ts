07c96a3206fd1bea344499fec84b8e8dc2ad2bee
SWtheWhite <swthewhite@gmail.com>
2023-02-19 12:07:38 +0900

    Changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,12 +9,78 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @ApiOperation({
-    summary: '검색',
-    description: '검색',
+    summary: '기본검색',
+    description: '기본검색',
   })
   @Get('search')
-  search(@Query('top_right_lat') top_right_lat:number, @Query('top_right_lon') top_right_lon:number, @Query('bottom_left_lat') bottom_left_lat:number, @Query('bottom_left_lon') bottom_left_lon:number, @Query('category') category:string[], @Query('text') text:string) {
-    return this.appService.search(top_right_lat, top_right_lon, bottom_left_lat, bottom_left_lon, category, text);
+  search(
+    @Query('top_right_lat') top_right_lat: number,
+    @Query('top_right_lon') top_right_lon: number,
+    @Query('bottom_left_lat') bottom_left_lat: number,
+    @Query('bottom_left_lon') bottom_left_lon: number,
+    @Query('category') category: string[],
+    @Query('text') text: string,
+  ) {
+    return this.appService.search(
+      top_right_lat,
+      top_right_lon,
+      bottom_left_lat,
+      bottom_left_lon,
+      category,
+      text,
+    );
+  }
+  @ApiOperation({
+    summary: '별점정렬',
+    description: '별점정렬',
+  })
+  @Get('withrating')
+  withrating(
+    @Query('top_right_lat') top_right_lat: number,
+    @Query('top_right_lon') top_right_lon: number,
+    @Query('bottom_left_lat') bottom_left_lat: number,
+    @Query('bottom_left_lon') bottom_left_lon: number,
+    @Query('category') category: string[],
+    @Query('text') text: string,
+  ) {
+    return this.appService.withrating(
+      top_right_lat,
+      top_right_lon,
+      bottom_left_lat,
+      bottom_left_lon,
+      category,
+      text,
+    );
+  }
+  @ApiOperation({
+    summary: '거리정렬',
+    description: '거리정렬',
+  })
+  @Get('withdistance')
+  withdistance(
+    @Query('top_right_lat') top_right_lat: number,
+    @Query('top_right_lon') top_right_lon: number,
+    @Query('bottom_left_lat') bottom_left_lat: number,
+    @Query('bottom_left_lon') bottom_left_lon: number,
+    @Query('category') category: string[],
+    @Query('text') text: string,
+  ) {
+    return this.appService.withdistance(
+      top_right_lat,
+      top_right_lon,
+      bottom_left_lat,
+      bottom_left_lon,
+      category,
+      text,
+    );
+  }
+  @ApiOperation({
+    summary: 'testsearch',
+    description: 'testsearch',
+  })
+  @Get('testsearch')
+  testsearch() {
+    return this.appService.testsearch();
   }
 
   @ApiOperation({
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,52 +3,195 @@ import client from './connection';
 
 @Injectable()
 export class AppService {
-  async search(top_right_lat, top_right_lon, bottom_left_lat, bottom_left_lon, category, text): Promise<unknown[]> {
-    const categories = category;
-
+  async search(
+    top_right_lat,
+    top_right_lon,
+    bottom_left_lat,
+    bottom_left_lon,
+    category,
+    text,
+  ): Promise<unknown[]> {
+    let temp = '';
+    if (category.length == 1) {
+      temp = category;
+    } else {
+      temp = category.join(' ');
+    }
+    const categories = temp;
+    console.log(categories);
     const elastic = client;
     try {
       const response = await elastic.search({
-        index: 'store_list',
+        index: 'test',
         query: {
-          "bool": {
-            "must": [
+          bool: {
+            must: [
               {
-                "match": {
-                  "category": categories[0]
-                }
-              }
+                match: {
+                  theme: categories,
+                },
+              },
             ],
-            "should": [
+            should: [
               {
-                "match": {
-                  "name": text
-                }
-              }
+                match: {
+                  name: text,
+                },
+              },
             ],
-            "filter": {
-              "geo_bounding_box": {
-                "location": {
-                  "top_right": {
-                    "lat": top_right_lat,
-                    "lon": top_right_lon
+            filter: {
+              geo_bounding_box: {
+                location: {
+                  top_right: {
+                    lat: top_right_lat,
+                    lon: top_right_lon,
                   },
-                  "bottom_left": {
-                    "lat": bottom_left_lat,
-                    "lon": bottom_left_lon
-                  }
-                }
-              }
-            }
-          }
-        }
+                  bottom_left: {
+                    lat: bottom_left_lat,
+                    lon: bottom_left_lon,
+                  },
+                },
+              },
+            },
+          },
+        },
       });
-      return response.hits.hits.map((hit) => hit._source);
+      return response.hits.hits.map((row) => {
+        return {
+          id: row._id,
+          source: row._source,
+        };
+      });
     } catch (error) {
       console.log(error);
     }
   }
 
+  async withrating(
+    top_right_lat,
+    top_right_lon,
+    bottom_left_lat,
+    bottom_left_lon,
+    category,
+    text,
+  ): Promise<unknown[]> {
+    const categories = category.join(' ');
+    console.log(categories);
+    const elastic = client;
+    try {
+      const response = await elastic.search({
+        index: 'store_list',
+        sort: [
+          {'naver_star': 'desc'},
+          '_score'
+        ],
+        query: {
+          match_all: {},
+        },
+      });
+      return response.hits.hits.map((row) => {
+        return {
+          id: row._id,
+          source: row._source,
+        };
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async withdistance(
+    top_right_lat,
+    top_right_lon,
+    bottom_left_lat,
+    bottom_left_lon,
+    category,
+    text,
+  ): Promise<unknown[]> {
+    const categories = category.join(' ');
+    console.log(categories);
+    const elastic = client;
+    try {
+      const response = await elastic.search({
+        index: 'store_list',
+        query: {
+          bool: {
+            must: [
+              {
+                match: {
+                  category: categories,
+                },
+              },
+            ],
+            should: [
+              {
+                match: {
+                  name: text,
+                },
+              },
+            ],
+            filter: {
+              geo_bounding_box: {
+                location: {
+                  top_right: {
+                    lat: top_right_lat,
+                    lon: top_right_lon,
+                  },
+                  bottom_left: {
+                    lat: bottom_left_lat,
+                    lon: bottom_left_lon,
+                  },
+                },
+              },
+            },
+          },
+        },
+      });
+      return response.hits.hits.map((row) => {
+        return {
+          id: row._id,
+          source: row._source,
+        };
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async testsearch(): Promise<unknown> {
+    const elastic = client;
+    try {
+      const response = await elastic.search({
+        index: 'store_list',
+        query: {
+          bool: {
+            filter: {
+              geo_bounding_box: {
+                location: {
+                  top_right: {
+                    lat: 37.56221127978163,
+                    lon: 126.93394472172852,
+                  },
+                  bottom_left: {
+                    lat: 37.526517175496835,
+                    lon: 126.84622575810548,
+                  },
+                },
+              },
+            },
+          }
+        },
+      });
+      return response.hits.hits.map((row) => {
+        return {
+          id: row._id,
+          source: row._source,
+        };
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
   async findAll(): Promise<unknown> {
     const elastic = client;
     try {
