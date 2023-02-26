2f6804fe9cd5b064dfff7c2bb4970c84b9d4b465
SWtheWhite <swthewhite@gmail.com>
2023-02-27 00:12:00 +0900

    UPDATE isopening fin & change function names

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,8 +9,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @ApiOperation({
-    summary: '기본검색',
-    description: '기본검색',
+    summary: 'search',
+    description: 'search with many filters',
   })
   @ApiImplicitQuery({
     name: 'category',
@@ -51,54 +51,53 @@ export class AppController {
     );
   }
   @ApiOperation({
-    summary: 'idsearch',
-    description: 'idsearch',
+    summary: 'info from ID',
+    description: 'get store information by using ID',
   })
-  @Get('idsearch')
-  idsearch(@Query('store_id') store_id: string) {
-    return this.appService.idsearch(store_id);
+  @Get('info-from-id')
+  infoFromId(@Query('store_id') store_id: string) {
+    return this.appService.infoFromId(store_id);
   }
   @ApiOperation({
-    summary: 'testsearch',
-    description: 'testsearch',
+    summary: 'is opening',
+    description: 'get store is opening or not',
   })
-  @Get('testsearch')
-  testsearch(
-    @Query('top_right_lat') top_right_lat: number,
-    @Query('top_right_lon') top_right_lon: number,
-    @Query('bottom_left_lat') bottom_left_lat: number,
-    @Query('bottom_left_lon') bottom_left_lon: number,
-  ) {
-    return this.appService.testsearch(
-      top_right_lat,
-      top_right_lon,
-      bottom_left_lat,
-      bottom_left_lon,
-    );
-  }
-  @ApiOperation({
-    summary: 'isopening',
-    description: 'isopening',
-  })
-  @Get('isopeing')
-  isopening(@Query('store_id') store_id: string) {
-    return this.appService.isopening(store_id);
+  @Get('is-opening')
+  isOpening(@Query('store_id') store_id: string) {
+    return this.appService.isOpening(store_id);
   }
   @ApiOperation({
     summary: '모두 가져오기',
     description: '모두 가져오기',
   })
-  @Get('findAll')
+  @Get('find-all')
   findOneById() {
     return this.appService.findAll();
   }
-
   @ApiOperation({
-    summary: '테스트',
-    description: '테스트',
+    summary: 'check connection',
+    description: 'check ElasticSearch Connection',
   })
-  @Get('test')
-  test() {
-    return this.appService.test();
+  @Get('connection')
+  elasticConnection() {
+    return this.appService.elasticConnection();
+  }
+  @ApiOperation({
+    summary: 'test search',
+    description: 'search for test',
+  })
+  @Get('testsearch')
+  testSearch(
+    @Query('top_right_lat') top_right_lat: number,
+    @Query('top_right_lon') top_right_lon: number,
+    @Query('bottom_left_lat') bottom_left_lat: number,
+    @Query('bottom_left_lon') bottom_left_lon: number,
+  ) {
+    return this.appService.testSearch(
+      top_right_lat,
+      top_right_lon,
+      bottom_left_lat,
+      bottom_left_lon,
+    );
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -828,7 +828,101 @@ export class AppService {
       }
     }
   }
-  async testsearch(
+
+  async infoFromId(store_id): Promise<unknown> {
+    const elastic = client;
+    try {
+      const response = await elastic.get({
+        index: 'store_list',
+        id: store_id,
+      });
+      return { id: response._id, source: response._source };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async isOpening(store_id): Promise<unknown> {
+    const weekday = new Array(7);
+    weekday[0] = '월';
+    weekday[1] = '화';
+    weekday[2] = '수';
+    weekday[3] = '목';
+    weekday[4] = '금';
+    weekday[5] = '토';
+    weekday[6] = '일';
+    const d = new Date();
+    let index = d.getDay() - 1;
+    if (index < 0) {
+      index = 6;
+    }
+    const nowMinutes = d.getHours() * 60 + d.getMinutes();
+
+    const elastic = client;
+    try {
+      const response = await elastic.get({
+        index: 'store_list',
+        id: store_id,
+        _source: ['opening_hour', 'opening_breaktime'],
+      });
+      const today_opening_breaktime =
+        response._source['opening_breaktime'][index][weekday[index]];
+      const today_opening_hour =
+        response._source['opening_hour'][index][weekday[index]];
+      if (today_opening_breaktime != 'None') {
+        const timeline = today_opening_breaktime.split('-');
+        const start = timeline[0].split(':');
+        const startMinutes = Number(start[0]) * 60 + Number(start[1]);
+        const end = timeline[1].split(':');
+        const endMinutes = Number(end[0]) * 60 + Number(end[1]);
+        if (nowMinutes >= startMinutes && nowMinutes <= endMinutes) {
+          return 'breaktime';
+        }
+      }
+      if (today_opening_hour != 'None') {
+        const timeline = today_opening_hour.split('-');
+        const start = timeline[0].split(':');
+        const startMinutes = Number(start[0]) * 60 + Number(start[1]);
+        const end = timeline[1].split(':');
+        const endMinutes = Number(end[0]) * 60 + Number(end[1]);
+        if (nowMinutes >= startMinutes && nowMinutes <= endMinutes) {
+          return 'open';
+        } else {
+          return 'closed';
+        }
+      }
+      return 'None';
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async findAll(): Promise<unknown> {
+    const elastic = client;
+    try {
+      const response = await elastic.search({
+        size: 100,
+        index: 'store_list',
+        query: {
+          match_all: {},
+        },
+      });
+      return response.hits.hits.map((hit) => {
+        return {
+          id: hit._id,
+          source: hit._source,
+        };
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async elasticConnection() {
+    try {
+      return await client.cluster.health({});
+    } catch (error) {
+      return error;
+    }
+  }
+  async testSearch(
     top_right_lat,
     top_right_lon,
     bottom_left_lat,
@@ -868,60 +962,6 @@ export class AppService {
       });
     } catch (error) {
       console.log(error);
-    }
-  }
-  async idsearch(store_id): Promise<unknown> {
-    const elastic = client;
-    try {
-      const response = await elastic.get({
-        index: 'store_list',
-        id: store_id,
-      });
-      return { id: response._id, source: response._source };
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  async isopening(store_id): Promise<unknown> {
-    const elastic = client;
-    try {
-      const response = await elastic.get({
-        index: 'store_list',
-        id: store_id,
-        _source: ['opening_hour', 'opening_breaktime', 'opening_lastorder'],
-      });
-      console.log(response._source);
-      //return { id: response._id, source: response._source };
-      return 'open';
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  async findAll(): Promise<unknown> {
-    const elastic = client;
-    try {
-      const response = await elastic.search({
-        size: 100,
-        index: 'store_list',
-        query: {
-          match_all: {},
-        },
-      });
-      return response.hits.hits.map((hit) => {
-        return {
-          id: hit._id,
-          source: hit._source,
-        };
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  async test() {
-    try {
-      return await client.cluster.health({});
-    } catch (error) {
-      return error;
     }
   }
 }
